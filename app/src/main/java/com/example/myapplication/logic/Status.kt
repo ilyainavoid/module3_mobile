dbd0a674f1a5e699e0ddf4302d38f0b111aaf422
Opadimasik <jthdthdfg@gmail.com>
2023-05-16 20:59:12 +0700

    //

--- app/src/main/java/com/example/myapplication/logic/Status.kt
+++ app/src/main/java/com/example/myapplication/logic/Status.kt
@@ -2,7 +2,9 @@ package com.example.myapplication.logic
 
 var OK = fun(): String { return "OK" }
 
+var invalidComparator = fun(): String { return "Invalid comparator" }
 var divisionByZero = fun(): String { return "Division by zero" }
 var incorrectExpression = fun(): String { return "Incorrect expression" }
 var incorrectNaming = fun(name: String): String { return "Incorrect naming '$name'" }
 var unexpectedSymbol = fun(operand: String): String { return "Unexpected symbol '$operand'" }
+var allComparators = listOf(">", ">=", "<", "<=", "==", "!=")
