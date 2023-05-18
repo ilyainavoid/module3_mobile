f7ba41a57cce59f46daca36774cda0deb8ef749d
andreivasilev <vasandrey007@gmail.com>
2023-05-19 02:47:23 +0700

    for arrays

--- app/src/main/java/com/example/myapplication/logic/RPN.kt
+++ app/src/main/java/com/example/myapplication/logic/RPN.kt
@@ -4,19 +4,19 @@ import java.util.*
 import kotlin.math.pow
 
 fun calculate(container: Container, expression: String): Pair<String,Int> {
-    val exp = expression.replace(" ", "")
+    val exp = expression.replace("\\s".toRegex(), "")
     if (exp.isEmpty()){
-        return Pair(" ", 0)
+        return Pair(emptyInput(), 0)
     }
     val (prepared, expStatus) = preparingExpression(container, exp)
-    val lineStatus = lineCheck(exp)
-    if (lineStatus == 0) {
-        return Pair(" ", 0)
+    val (lineStatus, correctLine) = lineCheck(exp)
+    if (correctLine == 0) {
+        return Pair(emptyInput(), 0)
     }
     if (expStatus == 0) {
-        return Pair(" ", 0)
+        return Pair(lineStatus, 0)
     }
-    return Pair(OK(), RPNtoAnswer(toRPN(prepared)))
+    return RPNtoAnswer(toRPN(prepared))
 }
 
 private fun getPriority(symbol:Char):Int{
@@ -63,7 +63,7 @@ private fun toRPN(expression: String): String{
     return current
 }
 
-private fun RPNtoAnswer(RPN: String): Int{
+private fun RPNtoAnswer(RPN: String): Pair<String, Int>{
     var operand = String()
     val stack: Stack<Int> = Stack<Int>()
     var i = 0
@@ -80,7 +80,7 @@ private fun RPNtoAnswer(RPN: String): Int{
             try {
                 stack.push(operand.toInt())
             } catch (e: NumberFormatException) {
-                return 0
+                return Pair(unexpectedSymbol(operand), 0)
             }
             operand = String()
         }
@@ -97,37 +97,55 @@ private fun RPNtoAnswer(RPN: String): Int{
                         try {
                             stack.push(b / a)
                         } catch (e: Exception) {
-                            return 0
+                            return Pair(divisionByZero(), 0)
                         }
                     }
                     '%' -> {
                         try {
                             stack.push(b % a)
                         } catch (e: Exception) {
-                            return 0
+                            return Pair(divisionByZero(), 0)
                         }
                     }
                     else -> {
-                        return  0
+                        return  Pair(unexpectedSymbol(RPN[i].toString()), 0)
                     }
                 }
             } catch (e: EmptyStackException) {
-                return 0
+                return Pair(incorrectExpression(), 0)
             }
         }
         i++
     }
-    return stack.pop()
+    return Pair(OK(), stack.pop())
 }
 
 private fun preparingExpression(container: Container, expression: String): Pair<String, Int> {
     var exp = expression
     var preparedExpression = String()
+    val regArr = "([A-Za-z]+[A-Za-z0-9_]*)\\[[A-Za-z0-9 +%*/_#^-]*]".toRegex()
+    var array = regArr.find(exp)
+    while (array != null) {
+        val (arrName, arrIndex) = indexCount(container, array.value)
+        if (arrIndex == -1) {
+            return Pair(arrName, 0)
+        }
+        if (!container.isArrayExist(arrName)) {
+            return Pair(undefinedArray(arrName), 0)
+        }
+        val arrValue = container.getArrayValue(arrName, arrIndex)
+        var fromArrToNum = arrValue.toString()
+        if (arrValue!!.toInt() < 0) {
+            fromArrToNum = "($fromArrToNum)"
+        }
+        exp = exp.replace(array.value, fromArrToNum)
+        array = regArr.find(exp)
+    }
     val reg = "[A-Za-z]+[A-Za-z0-9_]*".toRegex()
     var variable = reg.find(exp)
     while (variable != null) {
         if (!container.isVariableExist(variable.value)) {
-            return Pair("", 0)
+            return Pair(undefinedVariable(variable.value), 0)
         }
         val varValue = container.getVariableValue(variable.value)
         var fromVarToNum = varValue.toString()
@@ -149,32 +167,71 @@ private fun preparingExpression(container: Container, expression: String): Pair<
     return Pair(preparedExpression, 1)
 }
 
-private fun lineCheck(string: String): Int {
+private fun lineCheck(string: String): Pair<String, Int> {
     var str = string.replace("[A-Za-z-+*/0-9()%_\\[\\]]".toRegex(), "")
     if (str.isNotEmpty()) {
-        return 0
+        return Pair(unexpectedSymbol(str), 0)
     }
     val reg =
         "([-+% ]+[0-9_]+[A-Za-z_]+[0-9]*[-+%* ]*)|(\\b[0-9_]+[A-Za-z_]+[0-9]*)|(\\b[_][0-9]+)".toRegex()
     if (reg.find(string) != null) {
-        return 0
+        return Pair(incorrectExpression(), 0)
     }
     str = string.replace("[A-Za-z-+*/0-9%^#_\\[\\]]".toRegex(), "")
     val bracket1 = str.replace("\\(".toRegex(), "")
     val bracket2 = str.replace("\\)".toRegex(), "")
     if (bracket1.length != bracket2.length) {
-        return 0
+        return Pair(incorrectExpression(), 0)
     }
-    return 1
+    return Pair(OK(), 1)
 }
 
-fun defineInput(heap: Container, expression: String): Pair<String, String> {
+fun defineInput(heap: Container, expression: String): Triple<String, String, Int> {
+    val arr = "[A-Za-z]+[\\[(\\d+_*^#)\\]]".toRegex()
     val variable = "[A-Za-z]+[A-Za-z0-9_]*".toRegex()
 
-    if (variable.find(expression) != null) {
-        if (heap.isVariableExist(expression)) {
-            return Pair("Var", expression)
+    if (arr.find(expression) != null) {
+        val (name, index) = indexCount(heap, expression)
+        if (index == -1) {
+            return Triple(name, tagNothing(), 0)
+        }
+        if (heap.isArrayExist(name) && index >= 0 && index < heap.getArraySize(name)!!.toInt()) {
+            return Triple(tagArray(), name, index)
         }
     }
-    return Pair(inputError(), "")
+    if (variable.find(expression) != null) {
+        if (heap.isVariableExist(expression)) {
+            return Triple(tagVariable(), expression, 0)
+        }
+    }
+    return Triple(inputError(), tagNothing(), 0)
+}
+
+private fun indexCount(container: Container, arr: String): Pair<String, Int> {
+    var array = arr
+    var index = -1
+    var arrayName = ""
+    val reg = "([A-Za-z]+[A-Za-z0-9_]*)\\[[A-Za-z0-9 +*/%_^#-]*]".toRegex()
+    while (reg.find(array) != null) {
+        val arg = "\\[[A-Za-z0-9 +*/_%^#-]*]".toRegex().find(array)
+        arrayName = "[A-Za-z]+[A-Za-z0-9_]*".toRegex().find(reg.find(array)!!.value)!!.value
+        if (arg != null) {
+            val arm = arg.value.replace("[", "").replace("]", "")
+            val (status, result) = calculate(container, arm)
+            array = array.replace(arm, result.toString())
+            index = result
+            if (!container.isArrayExist(arrayName)) {
+                return Pair(undefinedArray(arrayName), -1)
+            }
+            if (index >= container.getArraySize(arrayName)!!.toInt() || index < 0) {
+                return Pair(indexOutOfRange(), -1)
+            }
+            if (status != OK()) {
+                return Pair(status, -1)
+            }
+            val arrayValue = container.getArrayValue(arrayName, result)
+            array = array.replace(reg.find(array)!!.value, arrayValue.toString())
+        }
+    }
+    return Pair(arrayName, index)
 }
